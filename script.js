bc2013ca63011c184f4f21106acc2a5b2ebaaf8c
Lawson Nuland <lawson.nuland@gmail.com>
2022-02-18 13:11:52 -0500

    Calculator now functions appropriately, and handles some edge cases.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let firstNumber;
 let secondNumber;
 let displayContent = "";
+let result = 0;
+let operatorPresent = false;
 const display = document.querySelector('#calculationText');
 const digits = document.querySelectorAll('.digit');
 const operators = document.querySelectorAll('.operator');
@@ -15,8 +17,18 @@ digits.forEach((digit) => {
 
 operators.forEach((operator) => {
     operator.addEventListener(('click'), () => {
-        displayContent += operator.value;
-        updateDisplay();
+        if (operatorPresent == false) {
+            displayContent += operator.value;
+            updateDisplay();
+            operatorPresent = true;
+        } else {
+            resolve(); 
+            if (operatorPresent == false) {
+                displayContent += operator.value;
+                operatorPresent = true;
+            }
+            updateDisplay();
+        }
     });
 });
 
@@ -24,35 +36,40 @@ clearButton.addEventListener(('click'), () => {
     clear();
 });
 
-function add(a,b) {
+function add(a, b) {
     return a + b
 }
 
-function subtract(a,b) {
+function subtract(a, b) {
     return a - b
 }
 
-function multiply(a,b) {
+function multiply(a, b) {
     return a * b
 }
 
-function divide(a,b) {
+function divide(a, b) {
     return a / b
 }
 
-function operate(a,b,operation) {
-    switch(operation) {
-        case 'add': 
-            add(a,b);
+function operate(a, b, operation) {
+    switch (operation) {
+        case '+':
+            result = add(a, b);
             break;
-        case 'subtract': 
-            subtract(a,b);
+        case '-':
+            result = subtract(a, b);
             break;
-        case 'multiply': 
-            multiply(a,b);
+        case '*':
+            result = multiply(a, b);
             break;
-        case 'divide': 
-            divide(a,b);
+        case '/':
+            if (b != 0) {
+                result = divide(a, b);
+            } else {
+                alert("Nice try buddy. Don't divide by zero.")
+                clear();
+            }
             break;
     }
 }
@@ -64,6 +81,30 @@ function updateDisplay() {
 function clear() {
     firstNumber = 0;
     secondNumber = 0;
-    displayContent="";
+    displayContent = "";
+    operatorPresent = false;
+    result = 0;
     updateDisplay();
+}
+
+function resolve() {
+    if (operatorPresent == true) {
+        let equationArray = displayContent.split(' ');
+        console.log(equationArray);
+        if (equationArray.length > 2 && equationArray[2] != '') {
+            firstNumber = parseFloat(equationArray[0]);
+            console.log(firstNumber);
+            secondNumber = parseFloat(equationArray[2]);
+            console.log(secondNumber);
+            operate(firstNumber, secondNumber, equationArray[1]);
+            console.log(result);
+            displayContent = result;
+            operatorPresent = false;
+            updateDisplay();
+        } else {
+            alert("You need 2 numbers and an operator for this to work!");
+        }
+    } else {
+        alert("You need 2 numbers and an operator for this to work!");
+    }
 }
